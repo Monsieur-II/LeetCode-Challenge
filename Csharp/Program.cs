5cb2c35140b3417eaef1f1404fba08ffafa2954f
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-09-23 17:30:10 +0000

    Two sum problem

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Problems = Csharp.LeetCodeChallenge;
 
-Console.WriteLine("Hello, World!");
+var answer1 = Problems.Two_Sum([2, 7, 11, 15], 9);
+Console.WriteLine(string.Join(',', answer1));
+
+var answer2 = Problems.Two_Sum([3, 2, 4], 6);
+Console.WriteLine(string.Join(',', answer2));
+
+var answer3 = Problems.Two_Sum([3, 3], 6);
+Console.WriteLine(string.Join(',', answer3));
